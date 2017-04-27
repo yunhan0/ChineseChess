95f086f789d39a0e921604670b8f36a5c77f7ef9
yunhan0 <yunhn.lee@gmail.com>
2017-04-27 17:48:31 +1000

    init the chess mvc

---
+++ ChineseChess/ChessView.swift
@@ -0,0 +1,21 @@
+//
+//  ChessView.swift
+//  ChineseChess
+//
+//  Created by Yunhan Li on 4/25/17.
+//  Copyright © 2017 Yunhan Li. All rights reserved.
+//
+
+import UIKit
+
+class ChessView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ ChineseChess/ChessViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChineseChess
+//
+//  Created by Yunhan Li on 4/25/17.
+//  Copyright © 2017 Yunhan Li. All rights reserved.
+//
+
+import UIKit
+
+class ChessViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
